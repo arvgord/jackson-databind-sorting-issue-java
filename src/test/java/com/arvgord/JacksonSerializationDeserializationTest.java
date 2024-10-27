26d350ded000bbb0cffba24954b22bd022c736e1
Artem Gordienko <arvgord@gmail.com>
2024-10-27 14:12:13 +0500

    refactor: change test name

--- src/test/java/com/arvgord/JacksonSerializationDeserializationTest.java
+++ src/test/java/com/arvgord/JacksonSerializationDeserializationTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class JacksonSortingTest {
+public class JacksonSerializationDeserializationTest {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
