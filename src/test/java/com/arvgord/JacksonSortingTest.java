f34fcb20f0c5136f6b4e304e1f64c1bc8eb9fb4f
Artem Gordienko <arvgord@gmail.com>
2024-10-29 13:02:44 +0500

    refactor: change tests for clear understanding sorting issue with field ordering

--- src/test/java/com/arvgord/JacksonSortingTest.java
+++ src/test/java/com/arvgord/JacksonSortingTest.java
@@ -31,55 +31,11 @@ public class JacksonSortingTest {
         }
     """;
 
-    private final String SECOND_UNEXPECTED_SORTING_JSON_OUTPUT = """
-        {
-            "transactionId": "test",
-            "a": 1,
-            "b": 2,
-            "c": [
-                {
-                    "id": "3",
-                    "value": "c"
-                },
-                {
-                    "id": "1",
-                    "value": "a"
-                },
-                {
-                    "id": "2",
-                    "value": "b"
-                }
-            ]
-        }
-    """;
-
-    private final String THIRD_UNEXPECTED_SORTING_JSON_OUTPUT = """
-        {
-            "transactionId": "test",
-            "c": [
-                {
-                    "id": "3",
-                    "value": "c"
-                },
-                {
-                    "id": "1",
-                    "value": "a"
-                },
-                {
-                    "id": "2",
-                    "value": "b"
-                }
-            ],
-            "a": 1,
-            "b": 2
-        }
-    """;
-
-    private <T> void testSerializationDeserialization(String outputResult, Class<T> clazz) throws Exception {
+    private <T> void testSerializationDeserialization(Class<T> clazz) throws Exception {
         T deserializedObject = objectMapper.readValue(INPUT_JSON, clazz);
         String serializedJson = objectMapper.writeValueAsString(deserializedObject);
 
-        String expectedJson = objectMapper.readTree(outputResult).toPrettyString();
+        String expectedJson = objectMapper.readTree(INPUT_JSON).toPrettyString();
         String actualJson = objectMapper.readTree(serializedJson).toPrettyString();
 
         assertEquals(expectedJson, actualJson);
@@ -87,16 +43,16 @@ public class JacksonSortingTest {
 
     @Test
     public void testSerializationAndDeserializationForFirstObject() throws Exception {
-        testSerializationDeserialization(INPUT_JSON, FirstObject.class);
+        testSerializationDeserialization(FirstObject.class);
     }
 
     @Test
     public void testSerializationAndDeserializationForSecondObject() throws Exception {
-        testSerializationDeserialization(SECOND_UNEXPECTED_SORTING_JSON_OUTPUT, SecondObject.class);
+        testSerializationDeserialization(SecondObject.class);
     }
 
     @Test
     public void testSerializationAndDeserializationForThirdObject() throws Exception {
-        testSerializationDeserialization(THIRD_UNEXPECTED_SORTING_JSON_OUTPUT, ThirdObject.class);
+        testSerializationDeserialization(ThirdObject.class);
     }
 }
